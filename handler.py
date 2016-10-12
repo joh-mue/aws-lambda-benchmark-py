27f032078e45c80bfb0871276c0937c3997abcfe
joh-mue <yesiamkeen@gmail.com>
2016-10-12 14:29:17 +0200

    handler skeleton, iamRoleStatements

--- handler.py
+++ handler.py
@@ -1,4 +1,28 @@
 import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def runBenchmark(event, context):
-    return { "message": "Go Serverless v1.0! Your function executed successfully!", "event": event }
+    start = context.get_remaining_time_in_millis()
+    response = { "test": "v0.0.1" , "event": event }
+
+    return response
+
+def createPayload():
+    logger.info("Creating Payload.")
+
+def deployPayload():
+    logger.info("Deploying Payload.")
+
+def deployPayloadToS3():
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucketName)
+    for 
+
+def retrievePayload():
+    logger.info("Retrieving Payload.")
+
+def invokingPayload():
+    logger.info("Retrieving Payload.")
